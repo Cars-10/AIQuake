e45fe3e13ca6155b7637c2ec6cee0c55bf1294bf
Ant Aydar <antaydar@gmail.com>
2023-10-06 12:33:46 +0200

    random forest model

--- src/models.py
+++ src/models.py
@@ -35,4 +35,26 @@ def make_and_return_model(df_train_engineered, df_label_balanced):
     # Test-train split
     X_train, X_test, y_train, y_test = train_test_split(df_train_engineered, df_label_balanced["damage_grade"], test_size=0.2, random_state=42)
 
-    return X_train, X_test, y_train, y_test, model01(X_train, y_train)
+    return X_train, X_test, y_train, y_test, train_random_forest(X_train, y_train)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+def train_random_forest(X_train, y_train, n_estimators=100, random_state=42):
+    """Train a Random Forest model and return it.
+
+    Args:
+        X_train (DataFrame): The feature data for training.
+        y_train (Series): The labels for training.
+        n_estimators (int): Number of trees in the forest.
+        random_state (int): Random seed for reproducibility.
+
+    Returns:
+        RandomForestClassifier: The trained Random Forest model.
+    """
+    
+    # Create and train the Random Forest model
+    clf = RandomForestClassifier(n_estimators=n_estimators, random_state=random_state)
+    clf.fit(X_train, y_train)
+    
+    return clf
