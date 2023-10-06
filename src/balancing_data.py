accf612fd2aa2f17da818a3c5810fd3b28e84f55
Johannes-Schall <jschall84@gmail.com>
2023-10-06 17:20:19 +0200

    geolocation encoding on geo1 and geo2

--- src/balancing_data.py
+++ src/balancing_data.py
@@ -1,2 +1,16 @@
 def balance_dataset(df_train, df_label):
+    """Balance the dataset by oversampling the minority classes.
+    First we find the number of classes and the number of samples in each class.
+    Then we find the class with the most samples.
+    Then we find the difference between the number of samples in the class with the most samples and the number of samples in each other class.
+    Then we oversample each class with less samples than the class with the most samples.
+    Finally we return the balanced dataset.
+
+
+    Args: df_train (dataframe): The dataframe with feature data.
+          df_label (dataframe): The dataframe with label data.
+
+    Returns: df_train_balanced (dataframe): The dataframe with balanced feature data.
+             df_label_balanced (dataframe): The dataframe with balanced label data."""
+
     return df_train, df_label
--- src/geolocation_encoding.py
+++ src/geolocation_encoding.py
@@ -1,22 +1,35 @@
-def encode_geolocation(df, colomn_to_encode=[
-                                             #'geo_level_3_id', 
-                                             #'geo_level_1_id', 
-                                             'geo_level_2_id'],
-                                             mean_damage_map=None,
-                                             df_label=None):
-    if mean_damage_map is None:
-        df['damage_grade'] = df_label['damage_grade']
+def encode_geolocation(
+    df,
+    colomn_to_encode=[
+        #'geo_level_3_id',
+        "geo_level_1_id",
+        "geo_level_2_id",
+    ],
+    mean_damage_maps=None,
+    df_label=None,
+):
+    if mean_damage_maps is None:
+        df["damage_grade"] = df_label["damage_grade"]
+        mean_damage_maps = {}
         for col in colomn_to_encode:
-            mean_damage_map = df.groupby(col)['damage_grade'].mean()
-            df[str(col + '_mean_damage')] = df[col].map(mean_damage_map)
-        df = df.drop(columns=['damage_grade'])
-        return df, mean_damage_map
+            mean_damage_map = df.groupby(col)["damage_grade"].mean()
+            # save mean_damage_map
+            mean_damage_maps[col] = mean_damage_map
+            df[str(col + "_mean_damage")] = df[col].map(mean_damage_map)
+        df = df.drop(columns=["damage_grade"])
+        return df, mean_damage_maps
     else:
         for col in colomn_to_encode:
-            df[str(col + '_mean_damage')] = df[col].map(mean_damage_map)
+            df[str(col + "_mean_damage")] = df[col].map(mean_damage_maps[col])
             # Count the NaN values in the column and print the result
-            n_NaN = df[str(col + '_mean_damage')].isna().sum()
-            print('{} column has {} NaN values, which getting filled with mean {}'.format(str(col + '_mean_damage'), n_NaN, mean_damage_map.mean()))
+            n_NaN = df[str(col + "_mean_damage")].isna().sum()
+            print(
+                "{} column has {} NaN values, which getting filled with mean {}".format(
+                    str(col + "_mean_damage"), n_NaN, mean_damage_maps[col].mean()
+                )
+            )
             # For all NaN values, replace with the mean damage grade
-            df[str(col + '_mean_damage')] = df[str(col + '_mean_damage')].fillna(mean_damage_map.mean())
-        return df, mean_damage_map
+            df[str(col + "_mean_damage")] = df[str(col + "_mean_damage")].fillna(
+                mean_damage_maps[col].mean()
+            )
+        return df, mean_damage_maps
